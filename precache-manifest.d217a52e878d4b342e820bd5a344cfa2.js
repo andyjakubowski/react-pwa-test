25ddf1388714a5f74844638eac6082c49b8a82ec
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-21 10:02:03 +0200

    Updates

---
+++ precache-manifest.d217a52e878d4b342e820bd5a344cfa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d91aac9c64352e3e77c75ed9c36e23",
+    "url": "/react-pwa-test/index.html"
+  },
+  {
+    "revision": "1b87443b407f4d12a875",
+    "url": "/react-pwa-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9cc90c724e560ffa8a87",
+    "url": "/react-pwa-test/static/js/2.58dd5c68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pwa-test/static/js/2.58dd5c68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b87443b407f4d12a875",
+    "url": "/react-pwa-test/static/js/main.5c55b762.chunk.js"
+  },
+  {
+    "revision": "7f8217c9395b29c14be7",
+    "url": "/react-pwa-test/static/js/runtime-main.932da4a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pwa-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
